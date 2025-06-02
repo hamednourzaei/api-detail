c13d3413ec2ac44ee5fe6ecc4907e962e7269912
hamed <52428565+hamednourzaei@users.noreply.github.com>
2025-06-02 12:22:58 +0330

    Update updateStats.js

--- scripts/updateStats.js
+++ scripts/updateStats.js
@@ -10,14 +10,14 @@ const stats = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 // به‌روزرسانی هر stat
 stats.forEach((stat) => {
   // افزایش مقدار اصلی
-  stat.value += 90;
+  stat.value += 10;
 
   // افزایش بازدید هر روز
   stat.chartData.forEach((data) => {
-    data.visits += 90;
+    data.visits += 12;
   });
 });
 
 // بازنویسی فایل با داده‌های جدید
 fs.writeFileSync(filePath, JSON.stringify(stats, null, 2));
-console.log("✅ Stats updated by +90.");
+console.log("✅ Stats updated by +12.");
